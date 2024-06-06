5416b73d0d1a28dd3c8cbdd5c4eba0b547198b13
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-06 13:58:03 +0100

    changed data and edited them

--- data/index.ts
+++ data/index.ts
@@ -1,217 +1,287 @@
 export const navItems = [
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Testimonials", link: "#testimonials" },
-    { name: "Contact", link: "#contact" },
-  ];
-  
-  export const gridItems = [
-    {
-      id: 1,
-      title: "I prioritize client collaboration, fostering open communication ",
-      description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.svg",
-      spareImg: "",
-    },
-    {
-      id: 2,
-      title: "I'm very flexible with time zone communications",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 3,
-      title: "My tech stack",
-      description: "I constantly try to improve",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-center",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 4,
-      title: "Tech enthusiast with a passion for development.",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
-    },
-  
-    {
-      id: 5,
-      title: "Currently building a JS Animation library",
-      description: "The Inside Scoop",
-      className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-      titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
-    },
-    {
-      id: 6,
-      title: "Do you want to start a project together?",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-      img: "",
-      spareImg: "",
-    },
-  ];
-  
-  export const projects = [
-    {
-      id: 1,
-      title: "3D Solar System Planets to Explore",
-      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-      img: "/p1.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-      link: "/ui.earth.com",
-    },
-    {
-      id: 2,
-      title: "Yoom - Video Conferencing App",
-      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-      img: "/p2.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "/ui.yoom.com",
-    },
-    {
-      id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "/ui.aiimg.com",
-    },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "/ui.apple.com",
-    },
-  ];
-  
-  export const testimonials = [
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-  ];
-  
-  export const companies = [
-    {
-      id: 1,
-      name: "cloudinary",
-      img: "/cloud.svg",
-      nameImg: "/cloudName.svg",
-    },
-    {
-      id: 2,
-      name: "appwrite",
-      img: "/app.svg",
-      nameImg: "/appName.svg",
-    },
-    {
-      id: 3,
-      name: "HOSTINGER",
-      img: "/host.svg",
-      nameImg: "/hostName.svg",
-    },
-    {
-      id: 4,
-      name: "stream",
-      img: "/s.svg",
-      nameImg: "/streamName.svg",
-    },
-    {
-      id: 5,
-      name: "docker.",
-      img: "/dock.svg",
-      nameImg: "/dockerName.svg",
-    },
-  ];
-  
-  export const workExperience = [
-    {
-      id: 1,
-      title: "Frontend Engineer Intern",
-      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-      className: "md:col-span-2",
-      thumbnail: "/exp1.svg",
-    },
-    {
-      id: 2,
-      title: "Mobile App Dev - JSM Tech",
-      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp2.svg",
-    },
-    {
-      id: 3,
-      title: "Freelance App Dev Project",
-      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-      className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp3.svg",
-    },
-    {
-      id: 4,
-      title: "Lead Frontend Developer",
-      desc: "Developed and maintained user-facing features using modern frontend technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
-  ];
-  
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/git.svg",
-    },
-    {
-      id: 2,
-      img: "/twit.svg",
-    },
-    {
-      id: 3,
-      img: "/link.svg",
-    },
-  ];
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Open to Collaborations",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Bube - A Brand Ecommerce Platform",
+    des: "A premier E-commerce platform tailored for the modern shopper built with Next.js and Clerk for authentication.",
+    img: "/project_1.png",
+    iconLists: [
+      "/next.svg",
+      "re.svg",
+      "/ts.svg",
+      "/c.svg",
+      "/tail.svg",
+      "/redux.svg",
+      "/paystack.svg",
+    ],
+    link: "https://bubeboutique.vercel.app",
+    github: "https://github.com/immanuellking/Bube-Boutique",
+  },
+  {
+    id: 2,
+    title: "Rently - A Real Estate Web App",
+    des: "The ultimate web app for apartment browsing and rentals. Explore a wide variety of apartments, view detailed listings, and find the rental that fits you.",
+    img: "/project_2.png",
+    iconLists: [
+      "/re.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/firebase.svg",
+      "/redux.svg",
+      "/paystack.svg",
+    ],
+    link: "https://rently-estate.netlify.app",
+    github: "https://github.com/immanuellking/Rently",
+  },
+  {
+    id: 3,
+    title: "Exclusive - Ecommerce Store",
+    des: "Whether you're looking for the latest fashion trends and accessories, Exclusive is your one-stop destination.",
+    img: "/project_3.png",
+    iconLists: ["/re.svg", "/tail.svg", "redux.svg"],
+    link: "https://excellent-store.netlify.app",
+    github: "https://github.com/immanuellking/Excellent_E-commerce_Store",
+  },
+  {
+    id: 4,
+    title: "HooBank - Modern Bank App",
+    des: "A landing page for a Modern Bank Application built with React and Tailwindcss.",
+    img: "/project_4.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "https://modern-business-bank.netlify.app",
+    github: "https://github.com/immanuellking/modern-bank-website",
+  },
+  {
+    id: 5,
+    title: "Pizza-HUT",
+    des: "A Web Application built for a pizza resturant with React, Tailwindcss and paystack for test payment.",
+    img: "/project_5.png",
+    iconLists: ["/re.svg", "/tail.svg", "/redux.svg", "/paystack.svg"],
+    link: "https://pizza-hut-99.netlify.app",
+    github: "https://github.com/immanuellking/Pizza-hut",
+  },
+  {
+    id: 6,
+    title: "CRAPPO - A Crypto Investment Website",
+    des: "A landing page for a Crypto Investment Platform Application built with React and AOS animation.",
+    img: "/project_6.png",
+    iconLists: ["/re.svg", "/tail.svg"],
+    link: "https://github.com/immanuellking/Crypto-website-landing-page",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Ebube's stands out for his thoughtful approach to frontend development. His ability to combine creativity and practicality produces websites that are visually gorgeous and run perfectly. His ability to translate difficult ideas into elegant solutions is well-liked by peers, making him an invaluable member of any team.",
+    name: "Michael O.",
+    title: "Frontend Developer",
+  },
+  {
+    quote:
+      "Ebube's is incredibly inspiring in his never-ending quest for frontend technology mastery. His projects are consistently innovative due to his proactive approach to learning and applying new trends. His desire to collaborate and share information is valued by teammates, who help to foster an innovative and cheerful work atmosphere.",
+    name: "Kizito K.",
+    title: "Product Designer",
+  },
+  {
+    quote:
+      "Working together with Ebube is enjoyable. His ability for problem-solving and effective communication make him an ideal front-end development teammate. I like his positive attitude, confidence and his constant willingness to go above and beyond to get outstanding outcomes.",
+    name: "Kelechi E.",
+    title: "Backend Developer",
+  },
+];
+
+export const techStack = [
+  {
+    id: 1,
+    name: "HTML5",
+    img: "/html5.svg",
+  },
+  {
+    id: 2,
+    name: "CSS3",
+    img: "/css.svg",
+  },
+  {
+    id: 3,
+    name: "Javascript",
+    img: "/javascript.svg",
+  },
+  {
+    id: 4,
+    name: "TailwindCss",
+    img: "/tailwind.svg",
+  },
+  {
+    id: 5,
+    name: "Typescript",
+    img: "/typescript.svg",
+  },
+  {
+    id: 6,
+    name: "Redux",
+    img: "/redux_2.svg",
+  },
+  {
+    id: 7,
+    name: "Git",
+    img: "/git_2.svg",
+  },
+  {
+    id: 8,
+    name: "Firebase",
+    img: "/firebase.svg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern - Losode",
+    desc: "Assisted in the development of a web-based E-commerce platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Freelancer - Upwork",
+    desc: "Developed Web app for cients using React and Tailwindcss.",
+    className: "md:col-span-2",
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance Web Dev Project",
+    desc: "Collaborated with a dev and a designer in developing a website using modern frontend technologies for a client.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 4,
+    title: "Intern - New Horizons",
+    desc: "Started web development Journey by learning modern frontend technologies",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/immanuellking",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://www.x.com",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/egbosimba-emmanuel-0a9569230/",
+  },
+];
