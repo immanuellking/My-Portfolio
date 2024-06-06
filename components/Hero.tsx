256e4f944678aaf59fd553bd22fb3f2a639dbf2e
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-06 13:49:23 +0100

    changed sub text reduced padding spacing change main text in the text generate effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-20 lg:pt-36">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -29,14 +29,16 @@ const Hero = () => {
         />
       </div>
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+        <div className="max-w-[90vw] md:max-w-2xl lg:max-w-[70vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next js
+            Let's create magic
           </h2>
 
           <TextGenerateEffect
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[35px] md:text-5xl lg:text-6xl"
+            // words="Transforming Concepts into Seamless User Experiences"
+            // words="I Craft Compelling Websites that Attract and Captivate Users"
+            words="Building Websites that Attract and Captivate Users"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
