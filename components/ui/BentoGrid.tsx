a3a6614278c6159f0f391f1890c1d514bbec924c
immanuellking <emmaebubeegbosi@gmail.com>
2024-06-09 01:06:05 +0100

    remove comment and added my gmail to be copied

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -64,7 +64,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "emmaebubeegbosi@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
@@ -76,7 +76,6 @@ export const BentoGridItem = ({
         className
       )}
       style={{
-        //   you can generate the color from here https://cssgradient.io/
         background: "rgb(4,7,29)",
         backgroundColor:
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
